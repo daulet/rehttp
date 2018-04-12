ba24bf8faac8b45346bbe9bcf4d0a0ff1d65c6cb
Daulet <daulet@zhanguzin.kz>
2018-04-11 17:16:51 -0700

    Fix default namespace

--- rehttp/Receiver.cs
+++ rehttp/Receiver.cs
@@ -1,15 +1,14 @@
-
-using System.IO;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
+using System.IO;
 
-namespace rehttp
+namespace Rehttp
 {
-    public static class Function1
+    public static class Receiver
     {
         [FunctionName("Function1")]
         public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
