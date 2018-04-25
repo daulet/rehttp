d7faf53c95bb199c02f144ec3c66e2266b2518bd
Daulet <daulet@zhanguzin.kz>
2018-04-25 16:06:09 -0700

    Add initial tests to verify request is stored in queue correctly

--- test/rehttp.UnitTests/ReceiverTests.cs
+++ test/rehttp.UnitTests/ReceiverTests.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
 using Moq;
+using Newtonsoft.Json;
 using RichardSzalay.MockHttp;
+using System;
+using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -27,14 +32,125 @@ namespace Rehttp.UnitTests
             mockHttp.Expect(method, targetUrl)
                     .Respond(HttpStatusCode.OK, new StringContent(string.Empty));
 
+            var queueMock = new Mock<CloudQueue>(MockBehavior.Strict, new Uri("http://localhost"));
+
             await Receiver.RunAsync(
                 new HttpRequestMessage(method, $"https://rehttp.me/r/{targetUrl}"),
                 targetUrl,
-                null,
+                queueMock.Object,
                 mockHttp.ToHttpClient(),
                 Mock.Of<ILogger>());
 
             mockHttp.VerifyNoOutstandingExpectation();
+            queueMock.VerifyAll();
+        }
+
+        [Theory]
+        [InlineData("DELETE")]
+        [InlineData("GET")]
+        [InlineData("HEAD")]
+        [InlineData("OPTIONS")]
+        [InlineData("POST")]
+        [InlineData("PUT")]
+        [InlineData("TRACE")]
+        public async Task RunAsync_FailureStatusCode_SomeRequestQueued(string httpMethod)
+        {
+            var method = new HttpMethod(httpMethod);
+            var targetUrl = "http://endpoint.io/path/to/media";
+
+            var mockHttp = new MockHttpMessageHandler();
+            mockHttp.Expect(method, targetUrl)
+                    .Respond(HttpStatusCode.InternalServerError);
+
+            var queueMock = new Mock<CloudQueue>(MockBehavior.Strict, new Uri("http://localhost"));
+            queueMock
+                .Setup(x => x.AddMessageAsync(
+                    It.IsAny<CloudQueueMessage>(), It.IsAny<TimeSpan?>(), It.IsAny<TimeSpan?>(), It.IsAny<QueueRequestOptions>(), It.IsAny<OperationContext>()))
+                .Returns(Task.CompletedTask);
+
+            await Receiver.RunAsync(
+                new HttpRequestMessage(method, $"https://rehttp.me/r/{targetUrl}"),
+                targetUrl,
+                queueMock.Object,
+                mockHttp.ToHttpClient(),
+                Mock.Of<ILogger>());
+
+            mockHttp.VerifyNoOutstandingExpectation();
+        }
+
+        [Theory]
+        [InlineData("DELETE")]
+        [InlineData("GET")]
+        [InlineData("HEAD")]
+        [InlineData("OPTIONS")]
+        [InlineData("POST")]
+        [InlineData("PUT")]
+        [InlineData("TRACE")]
+        public async Task RunAsync_FailureStatusCode_ExactlyOneRequestQueued(string httpMethod)
+        {
+            var method = new HttpMethod(httpMethod);
+            var targetUrl = "http://endpoint.io/path/to/media";
+
+            var mockHttp = new MockHttpMessageHandler();
+            mockHttp.Expect(method, targetUrl)
+                    .Respond(HttpStatusCode.InternalServerError);
+
+            var queueMock = new Mock<CloudQueue>(MockBehavior.Loose, new Uri("http://localhost"));
+
+            await Receiver.RunAsync(
+                new HttpRequestMessage(method, $"https://rehttp.me/r/{targetUrl}"),
+                targetUrl,
+                queueMock.Object,
+                mockHttp.ToHttpClient(),
+                Mock.Of<ILogger>());
+
+            mockHttp.VerifyNoOutstandingExpectation();
+            queueMock.Verify(
+                x => x.AddMessageAsync(
+                    It.IsAny<CloudQueueMessage>(), It.IsAny<TimeSpan?>(), It.IsAny<TimeSpan?>(), It.IsAny<QueueRequestOptions>(), It.IsAny<OperationContext>()),
+                Times.Once);
+        }
+
+        [Theory]
+        [InlineData("DELETE")]
+        [InlineData("GET")]
+        [InlineData("HEAD")]
+        [InlineData("OPTIONS")]
+        [InlineData("POST")]
+        [InlineData("PUT")]
+        [InlineData("TRACE")]
+        public async Task RunAsync_FailureStatusCode_CorrectRequestQueued(string httpMethod)
+        {
+            var content = Path.GetRandomFileName();
+            var method = new HttpMethod(httpMethod);
+            var targetUrl = "http://endpoint.io/path/to/media";
+
+            var mockHttp = new MockHttpMessageHandler();
+            mockHttp.Expect(method, targetUrl)
+                    .Respond(HttpStatusCode.InternalServerError);
+
+            var queueMock = new Mock<CloudQueue>(MockBehavior.Loose, new Uri("http://localhost"));
+
+            await Receiver.RunAsync(
+                new HttpRequestMessage(method, $"https://rehttp.me/r/{targetUrl}")
+                {
+                    Content = new StringContent(content),
+                },
+                targetUrl,
+                queueMock.Object,
+                mockHttp.ToHttpClient(),
+                Mock.Of<ILogger>());
+
+            mockHttp.VerifyNoOutstandingExpectation();
+            queueMock.Verify(x => x.AddMessageAsync(
+                It.Is<CloudQueueMessage>(m =>
+                    m.AsString == JsonConvert.SerializeObject(
+                        new Request()
+                        {
+                            Content = new StringContent(content),
+                            Destination = targetUrl,
+                        })),
+                It.IsAny<TimeSpan?>(), It.IsAny<TimeSpan?>(), It.IsAny<QueueRequestOptions>(), It.IsAny<OperationContext>()));
         }
     }
 }
