d6cffeda45a3d8bfbe29cfb75b5a5baef6648123
Daulet <daulet@zhanguzin.kz>
2018-05-23 20:30:47 -0700

    Copy Function app before triggering test

--- script/test.ps1
+++ script/test.ps1
@@ -9,9 +9,13 @@ if ($LastExitCode -ne 0) {
 Write-Host "Running integration test" -ForegroundColor Blue
 
 function Start-AzureFunction ([int]$port, [string]$workingDir) {
-	Start-Process func -ArgumentList "host start --port $port" -WorkingDirectory $workingDir -PassThru
+    Write-Host "Copying to tmp/$workingDir"
+    Copy-Item -Path $workingDir -Destination "tmp/$workingDir" -Recurse
+    Write-Host "Starting function at port $port from tmp/$workingDir"
+    Start-Process func -ArgumentList "host start --port $port" -WorkingDirectory "tmp/$workingDir" -PassThru
 }
 
+Start-Process redis-server -PassThru
 Start-AzureFunction 7072 -workingDir "src\rehttp\bin\Debug\netstandard2.0"
 Start-AzureFunction 7073 -workingDir "test\rehttp.Mocks\bin\Debug\netstandard2.0"
 
